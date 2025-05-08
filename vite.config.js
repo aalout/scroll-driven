4bcd58a9aef27786e8a55af4c8a43c01f90eb178
ilya.volkov <ilya.volkov@chulakov.com>
2025-05-08 12:59:39 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/scroll-animations-master/', // IMPORTANT: Change this to your GitHub repository name!
+  base: '/scroll-driven/', // Adjusted to match the GitHub Pages URL structure
 }); 
